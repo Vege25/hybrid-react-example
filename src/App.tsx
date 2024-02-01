3e0e9af69b2f0d299b9a4af71cfe748e361d6986
VeetiSo <v.sorakivi@gmail.com>
2024-02-01 13:17:11 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Home from './Home';
 
 const App = () => {
+  let text = 'Hello world!';
+  const updateText = () => {
+    text = 'Hello world! Updated';
+    console.log(text);
+  };
   return (
     <>
-      <h1>My app</h1>
+      <h1 onClick={updateText}>{text}</h1>
       <Home />
     </>
   );
